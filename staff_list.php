80330c1f1e78bc60642aab1582289a83bdbd2819
masaki kamiizumi <nobutuna@masakinoMacBook-ea.local>
2018-07-07 18:20:33 +0900

    ADD Staff List Page

---
+++ staff_list.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ロクマル農園</title>
+</head>
+<body>
+
+<?php
+
+try{
+$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+$user= 'nobutuna';
+$password = 'Nobutuna9';
+
+$dbh = new PDO($dsn, $user, $password);
+$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+$sql = 'SELECT name FROM mst_staff WHERE 1'; 
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+$dbh = null ;
+
+print 'スタッフ一覧<br><br>';
+while(true){
+	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+	if($rec==false) break;
+	print '・' .$rec['name'];
+	print'<br>';
+}
+}catch(Exception $e){
+	print 'ただいま障害により大変ご迷惑をおかけしてみます。';
+	exit();
+}
+?>
+
+</body>
+</html>
